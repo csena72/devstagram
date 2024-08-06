16fc5ad9d1a03203cf9316c1c2e5d65165234163
Cristian Sena <csena@gett.mobi>
2024-08-06 10:50:23 -0300

    muestra imagen de perfil

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
 <div class="flex justify-center">
     <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
         <div class="w-8/12 lg:w-6/12 px-5">
-            <img src="{{ asset('img/usuario.svg') }}" alt="Imagen del usuario" />
+            @if($user->imagen)
+                <img src="{{ asset('perfiles').'/' . $user->imagen }}" alt="Imagen del usuario" />
+            @else
+                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen del usuario" />
+            @endif
         </div>
         <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
 
